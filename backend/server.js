19a3678f83c90ec49641daed1ec265f71b834beb
priyatham87 <saipriyatham016@gmail.com>
2025-01-06 00:06:50 +0530

    Twitter-clone

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import notificationRoutes from './routes/notificationRoutes.js';
 
 import connectMongoDB from './db/connectMongodb.js';
 
-dotenv.config();
+require('dotenv').config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
